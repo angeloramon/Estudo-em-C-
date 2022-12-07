4d0b5e2a28479ef36fff0537ac80f61e2b6def61
Ângelo Ramon <angelopessoaramon@gmail.com>
2022-12-07 16:57:37 -0300

    Primeiro Commit C#

---
+++ Program.cs
@@ -0,0 +1,475 @@
+﻿using ExemploFundamentos.Models;
+
+
+
+
+
+
+
+
+
+
+
+
+
+//(ESTRUTURA DE REPETIÇÃO: WHILE)
+
+// int numero = 5;
+// int contador = 0;
+
+// while (contador <= 10)
+// {
+//     Console.WriteLine($"{contador + 1}° Execução: {numero} x {contador} = {numero * contador}");
+//     contador++;
+
+//     if (contador == 6)
+//     {
+//         break;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//(ESTRUTURA DE REPETIÇÃO: FOR)
+// int numero = 5;
+
+// for(int contador = 0; contador <= 10; contador++)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Calculadora calc = new Calculadora();
+
+// // calc.Somar(10, 30);
+// // calc.Subtrair(10, 50);
+// // calc.Multiplicar(15, 45);
+// // calc.Dividir(2 ,2);
+// //calc.Potencia(3 , 3);
+// // calc.Seno(30);
+// // calc.Coseno(30);
+// // calc.Tangente(30);
+
+// calc.RaizQuadrada(9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bool choveu = false;
+// bool estaTarde = true;
+
+// Console.WriteLine(choveu == estaTarde);
+
+// if (!choveu && !estaTarde)
+// {
+//     Console.WriteLine("Vou pedalar");
+// }
+// else
+// {
+//     Console.WriteLine("Vou pedalar um outro dia");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bool possuiPresencaMinima = true;
+// double media = 6.5;
+
+// if (possuiPresencaMinima || media >= 7)
+// {
+//     Console.WriteLine("Aprovado!");
+// }
+// else
+// {
+//     Console.WriteLine("Reprovado!");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bool ehMaiorDeIdade = false;
+// bool possuiAutorizacaoDoResponsavel = false;
+
+// if (ehMaiorDeIdade || possuiAutorizacaoDoResponsavel)
+// {
+//     Console.WriteLine("Entrada Liberada");
+// }
+// else
+// {
+//     Console.WriteLine("Entrada não Liberada");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("Digite uma letra");
+// string letra = Console.ReadLine();
+
+
+// switch (letra)
+// {
+//     case "a": case "e": case "i": case "o": case "u":
+//         Console.WriteLine("Vogal");
+//         break;
+
+//     default:
+//         Console.WriteLine("Não é uma vogal");
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (letra == "a" ||
+//     letra == "e" ||
+//     letra == "i" ||
+//     letra == "o" ||
+//     letra == "u")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else
+// {
+//     Console.WriteLine("Não é uma vogal");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (letra == "a")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else if (letra == "e")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else if (letra == "i")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else if (letra == "o")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else if (letra == "u")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else 
+// {
+//     Console.WriteLine("Não é uma vogal");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int quantidadeEmEstoque = 3;
+// int quantidadeCompra = 0;
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+// Console.WriteLine($"Quantidade em Estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+// Console.WriteLine($"É possiível realizar a venda? {possivelVenda}");
+
+
+// if (quantidadeCompra == 0)
+// {
+//     Console.WriteLine("Venda inválida");
+// }
+
+// else if (possivelVenda)
+// {
+//     Console.WriteLine("Venda Realizada");
+// }
+// else
+// {
+//     Console.WriteLine("Desculpe, Não temos a quantidade desejada em estoque");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int a = 10;
+// int b = 20;
+
+// int c = a + b;
+
+// c = c + 5;
+
+// c += 5;
+
+//Console.WriteLine (c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual);
+
+
+
+
+
+
+
+
+
+
+
+
+// string Apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 1;
+
+// double altura = 1.80;
+
+// decimal preco = 1.80m;
+
+// bool condicao = true;
+
+
+// Console.WriteLine(Apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preco: " + preco);
+// Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
+
+/* Pessoa pessoa1 = new Pessoa ();
+
+pessoa1.Nome = "Buta";
+pessoa1.Idade = 26;
+pessoa1.Apresentar(); */
